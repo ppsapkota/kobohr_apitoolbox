e2dff4785b653075249df326f6846bcfed7d3f01
ppsapkota <sapkotapunya@hotmail.com>
2018-07-09 17:48:17 +0300

    Update

--- R/r_ps_kobo_api_xlsform_upload_kpi.R
+++ R/r_ps_kobo_api_xlsform_upload_kpi.R
@@ -34,75 +34,24 @@ kpi_url <- "https://kobo.humanitarianresponse.info/imports/"
 #STEP1 ----import xlsx form
 d_content<-kobohr_kpi_upload_xlsform(kpi_url,kobo_form_xlsx,kobo_user,Kobo_pw)
 import_url<-d_content$url
+
 #STEP2---getting the resulting asset UID---
 d_content<-kobohr_kpi_get_asset_uid(import_url,kobo_user,Kobo_pw)
 asset_uid <- d_content$messages$created$uid
+
 #STEP3 ----Deploy asset
 d_content<-kobohr_kpi_deploy_asset(asset_uid, kobo_user, Kobo_pw)
 
-#STEP1--- Uploading the XLSForm ------------
-# result<-httr::POST(url=kpi_url,
-#                     body=list(
-#                       file=upload_file(path=kobo_form_xlsx),
-#                       library = 'false'
-#                       ),
-#                       authenticate(kobo_user,Kobo_pw)
-#                   )
-# 
-# d_content <- rawToChar(result$content)
-# #print (d_content)
-# d_content <- fromJSON(d_content)
-# #--success - status = 'processing'
-# status<- d_content$status
-# f_url<-d_content$url
-# #STEP2---getting the resulting asset UID---
-#----Get the asset UID-------------------------
-#The asset UID is given by messages.created[0].uid in the JSON response when GETting an import. 
-#Multiple GETs may be required until the server indicates "status": "complete" in the response:
-#john@scrappy:/tmp/api_demo$ curl --silent --user jnm_api:test-for-punya --header 'Accept: application/json' 
-#https://kobo.humanitarianresponse.info/imports/iGYukk6NVEA64zwMsPtgRD/ | python -m json.tool
-# the URL is fetched from the output of previous step 
-#d_content <- kobohr_kpi_get_asset_uid(import_url,u,pw)
-# #supply url
-# result<-GET(f_url,authenticate(kobo_user,Kobo_pw),progress())
-# d_content <- rawToChar(result$content)
-# d_content <- fromJSON(d_content)
-# #--success - status = 'complete'
-# 
-# #STEP3-----Deploying the form------
-# #kpi_id <- d_content$uid
-# asset_uid <- d_content$messages$created$uid
-# url <-paste0("https://kobo.humanitarianresponse.info/assets/",asset_uid,"/deployment/")
-# d <- list(owner=paste0("https://kobo.humanitarianresponse.info/users/",kobo_user,"/"),active=TRUE)
-# #d<-list(owner=paste0("https://kobo.humanitarianresponse.info/users/",kobo_user,"/"),active=TRUE)
-# 
-# result<-httr::POST (url,
-#                     body=d,
-#                     authenticate(kobo_user,Kobo_pw)
-#                     )
-# 
-# d_content <- rawToChar(result$content)
-# print(d_content)
-# d_content <- fromJSON(d_content)
+#STEP4 ----Share asset
+content_object_i<-paste0("/assets/", asset_uid,"/")
+user_i<-paste0("/users/punya/")
+permission_list<-c("add_submissions","change_submissions","validate_submissions")
 
-#------------------------------------
-# #STEP1--- Uploading the XLSForm ------------
-# query<-list('form[file]'=upload_file(path=kobo_form_xlsx),
-#             'form[library]'=FALSE)
-# 
-# result<-httr::POST(url=kpi_url,
-#                    body = query,
-#                    authenticate(kobo_user,Kobo_pw)
-#                   )
-# 
-# d_content <- rawToChar(result$content)
-# #print (d_content)
-# d_content <- fromJSON(d_content)
-# #--success - status = 'processing'
-# 
-# f_url<-d_content$url
-# 
-
+for (permission_i in permission_list){
+  #permission_i<-"add_submissions"
+  d_content<-kobohr_kpi_share_asset(content_object_i, permission_i, user_i, kobo_user, Kobo_pw)
+  print (paste0(asset_uid,"--",d_content$user, "--",user_i,"--", permission_i))
+}
 
 
 
