081d845c8ed52e93d79805222fd5e8018a303967
ppsapkota <sapkotapunya@hotmail.com>
2018-07-09 17:21:11 +0300

    Update

--- R/r_ps_kobo_api_xlsform_upload_kpi_msna.R
+++ R/r_ps_kobo_api_xlsform_upload_kpi_msna.R
@@ -12,30 +12,40 @@ Kobo_pw<-""
 source ("./R/r_ps_kobo_library_init.R")
 #source ("./R/r_ps_kobo_authenticate.R") ###--not uploaded in Github - instead set Kobo_user and kobo_pw variables
 source ("./R/r_func_ps_kobo_utils.R")
-
+source ("./R/r_ps_kobo_authenticate.R")
 ###################---KPI--------#########################################
 kpi_url <- "https://kobo.humanitarianresponse.info/imports/"
 #----------------------------------#
 #kobo_form_xlsx<-"./xlsform/kobo_1701_NW.xlsx"
-kobo_form_xlsx_folder<-"./xlsform/tur/"
+kobo_form_xlsx_folder<-"./Data/xlsform/tur/"
+kobo_users_file<-"./Data/kobo_users/MSNA2018_TurkeyXB_coverage_summary_govpcode.xlsx"
+asset_uid_savename<-"./Data/kobo_users/asset_uid_tur.xlsx"
 ## get list of files
 f_list<-list.files(path=kobo_form_xlsx_folder,full.names=TRUE, ignore.case = TRUE,pattern = "xlsx|XLSX")
+#read KoBo users list
+d_kobo_users<-read_excel(path=kobo_users_file,sheet="data")
+
+#create empty data frame
 d_import_url<- data.frame(import_url=character(), 
-                           User=character(), 
+                           user=character(),
+                           f_name=character(),
                            stringsAsFactors=FALSE) 
 #IMPORT xlsx form
 for (kobo_form_xlsx in f_list){
-  #i_file<-f_list[1]
+  #kobo_form_xlsx<-f_list[1]
+  #get base name
+  kobo_form_xlsx_bn<-basename(kobo_form_xlsx)
   #STEP1 ----import xlsx form
   d_content<-kobohr_kpi_upload_xlsform(kpi_url,kobo_form_xlsx,kobo_user,Kobo_pw)
   import_url<-d_content$url
   #add to datafrme
-  d_import_url<-bind_rows(d_import_url,as.data.frame(import_url))
+  d_import_url<-bind_rows(d_import_url,data.frame(import_url=import_url,f_name=kobo_form_xlsx_bn))
 }
 
 #get ASSET UID
 d_asset_uid<- data.frame(asset_uid=character(), 
-                          User=character(), 
+                          user=character(),
+                          f_name=character(),
                           stringsAsFactors=FALSE) 
 #repeat through each import url to get asset uid
 for (i_row in 1:nrow(d_import_url)){
@@ -44,8 +54,22 @@ for (i_row in 1:nrow(d_import_url)){
   d_content<-kobohr_kpi_get_asset_uid(import_url,kobo_user,Kobo_pw)
   asset_uid <- d_content$messages$created$uid
   #
-  d_asset_uid<-bind_rows(d_asset_uid,as.data.frame(asset_uid))
+  d_asset_uid<-bind_rows(d_asset_uid,data.frame(asset_uid=asset_uid,f_name=d_import_url$f_name[i_row]))
 }
+
+##-------from the form name - identify partner ID and pull user name
+d_asset_uid$organization_code<-str_replace_all(d_asset_uid$f_name,"kobo_msna2018_","") %>% 
+                     str_sub(1,4)
+#bring kobo users
+d_kobo_users<-select(d_kobo_users,organization_code,kobo_user) %>% 
+              mutate(organization_code=as.character(organization_code))
+
+d_asset_uid<-d_asset_uid %>%
+             left_join(d_kobo_users,by=c("organization_code"="organization_code"))
+
+##--------exort asset uid
+openxlsx::write.xlsx(d_asset_uid,asset_uid_savename, sheetName="data")
+
 
 #DEPLOY ASSET
 for (i_row in 1:nrow(d_asset_uid)){
@@ -66,15 +90,26 @@ for (i_row in 1:nrow(d_asset_uid)){
 # )
 for (i_row in 1:nrow(d_asset_uid)){
   asset_uid<-d_asset_uid$asset_uid[i_row]
+  share_kobo_users<-d_asset_uid$kobo_user[i_row]
   #STEP3 ----Deploy asset
   content_object_i<-paste0("/assets/", asset_uid,"/")
-  user_i<-paste0("/users/", "punya","/")
-  permission_list<-c("add_submissions","change_submissions","validate_submissions")
   
-  for (permission_i in permission_list){
-    #permission_i<-"add_submissions"
-    d_content<-kobohr_kpi_share_asset(content_object_i, permission_i, user_i, kobo_user, Kobo_pw)
-    print (paste0(d_content$user, "--",permission_i))
+  ##---------prepare KoBo user list
+  d_share_kobo_users<-as.data.frame(unlist(strsplit(share_kobo_users,",")))
+  names(d_share_kobo_users)[1]<-"kobo_users"
+  #remove spaces
+  d_share_kobo_users$kobo_users<-str_replace_all(d_share_kobo_users$kobo_users," ","")
+  
+  #loop through all users
+  for (i_row_user in 1:nrow(d_share_kobo_users)){
+        user_i<-paste0("/users/", d_share_kobo_users$kobo_users[i_row_user],"/")
+        permission_list<-c("add_submissions","change_submissions","validate_submissions")
+        
+        for (permission_i in permission_list){
+          #permission_i<-"add_submissions"
+          d_content<-kobohr_kpi_share_asset(content_object_i, permission_i, user_i, kobo_user, Kobo_pw)
+          print (paste0(d_asset_uid$f_name[i_row],"--", asset_uid,"--",d_content$user, "--",user_i,"--", permission_i))
+        }
   }
 }
 
